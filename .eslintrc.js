8b2606e04e0547579f0d7fe87969d49feaa26dd8
mnao305 <naomasa305@gmail.com>
2019-10-01 23:22:51 +0900

    Lint設定変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/recommended',
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard',
     // https://prettier.io/docs/en/index.html
