369cd04d65f9443efe45eae890fc1071537eed64
mnao305 <naomasa305@gmail.com>
2019-10-02 12:33:55 +0900

    add: Twitterログイン

--- src/popup/router/pages/Login.vue
+++ src/popup/router/pages/Login.vue
@@ -6,13 +6,36 @@
 </template>
 
 <script>
+import auth from '@/plugins/auth'
+import { db } from '@/plugins/firestore'
+
 export default {
   data() {
     return {}
   },
   methods: {
-    twitterLogin() {
-      this.$router.push('/tweet')
+    async twitterLogin() {
+      try {
+        const data = await auth.twitterLogin()
+        const dbData = await db
+          .collection('users')
+          .doc(data.user.uid)
+          .get()
+
+        await db
+          .collection('users')
+          .doc(dbData.id)
+          .set(
+            {
+              accessToken: data.credential.accessToken,
+              secret: data.credential.secret,
+            },
+            { merge: true }
+          )
+        this.$router.push('/tweet')
+      } catch (error) {
+        this.errorMessage = 'ログインに失敗しました。'
+      }
     },
   },
 }
