c83e444703c3b74960159ef86d892048d596b5aa
mnao305 <naomasa305@gmail.com>
2019-10-02 17:34:14 +0900

    add: ツイート機能

--- src/popup/router/pages/Tweet.vue
+++ src/popup/router/pages/Tweet.vue
@@ -1,20 +1,52 @@
 <template>
   <div>
     <textarea v-model="tweetText" id="tweetText" name="tweetText" cols="30" rows="10" />
-    <button class="tweetBtn" @click="tweet">投稿</button>
+    <div id="subArea">
+      {{ message }}
+      <button class="tweetBtn" @click="tweet">Tweet</button>
+    </div>
   </div>
 </template>
 
 <script>
+import firebase from '@/plugins/firebase'
+import axios from 'axios'
 export default {
   data() {
     return {
       tweetText: '',
+      message: '',
+      timerID: null,
     }
   },
   methods: {
-    tweet() {
-      console.log(this.tweetText)
+    async tweet() {
+      try {
+        clearTimeout(this.timerID)
+        this.message = 'Tweet中...'
+        const token = await firebase.auth().currentUser.getIdToken()
+        await axios.post(
+          'https://asia-northeast1-exbeak.cloudfunctions.net/tweet',
+          { tweet: this.tweetText },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        this.tweetText = ''
+        this.message = 'Tweet完了'
+        this.timerID = setTimeout(() => {
+          this.message = ''
+        }, 2000)
+      } catch (error) {
+        console.error(error)
+        this.message = 'Tweet失敗'
+        this.timerID = setTimeout(() => {
+          this.message = ''
+        }, 2000)
+      }
     },
   },
 }
