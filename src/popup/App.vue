167bf8b5c09687ada504d68fbacd20f4c009fc3f
mnao305 <naomasa305@gmail.com>
2019-10-06 21:45:20 +0900

    設定によって画面サイズやフォントサイズを変える

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,20 +1,41 @@
 <template>
-  <div id="popup">
+  <div id="popup" :style="styles">
     <router-view />
   </div>
 </template>
 
 <script>
+import Browser from 'webextension-polyfill'
+
 export default {
   data() {
-    return {}
+    return {
+      styles: {
+        '--width': '320px',
+        '--fontSize': '12px',
+      },
+    }
+  },
+
+  async mounted() {
+    const data = await Browser.storage.local.get({
+      screenWidth: '320',
+      fontSize: '12',
+    })
+    this.styles['--width'] = `${data.screenWidth}px`
+    this.styles['--fontSize'] = `${data.fontSize}px`
   },
 }
 </script>
 
 <style lang="scss">
 #popup {
-  width: 320px;
+  --width: 320px;
+  --fontSize: 12px;
+  width: var(--width);
+  * {
+    font-size: var(--fontSize);
+  }
 }
 
 *,
